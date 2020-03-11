cbc5433c1569a5219448a9daa8f9e83a6350d660
azamkhan99 <azan_khan99@outlook.com>
2020-03-11 19:16:32 +0000

    TCPSender

--- app/src/main/java/com/solarbabes/heliopot/WifiSetup.java
+++ app/src/main/java/com/solarbabes/heliopot/WifiSetup.java
@@ -1,20 +1,35 @@
 package com.solarbabes.heliopot;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
 import android.widget.EditText;
 
 public class WifiSetup extends AppCompatActivity {
+    String IP = "192.168.5.1";
+    int port = 8765;
     private EditText ssid;
     private EditText password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_setup);
+
+        ssid = (EditText) findViewById(R.id.username);
+    }
+
+    public void send(View v) {
+        TCPSender tcpSender = new TCPSender();
+        tcpSender.execute(ssid.getText().toString());
+    }
+
+    public void goToPlantDetail(View view) {
+        Intent intent = new Intent(this, PlantDetail.class);
+        startActivity(intent);
     }
 }
